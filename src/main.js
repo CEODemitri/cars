9b1115b66d361ad680011f8ee6b3c8b99a0955cd
ceoDemitri <demichole@gmail.com>
2025-03-29 00:23:05 -0500

    refractored project with vite scaffold. start of something gorgeous

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+
+// Component Style for this Project?
+// import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Under Construction</h1>
+  </div>
+`;
+
+// setupCounter(document.querySelector("#counter"));
