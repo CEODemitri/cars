592316f587eb8e5b75b51dc1df8d4278cc120ce0
ceoDemitri <demichole@gmail.com>
2025-04-19 19:07:04 -0500

    reframing home landing page. placed hero section with 100vh. start of locations section

--- src/main.js
+++ src/main.js
@@ -7,7 +7,32 @@ import viteLogo from "/vite.svg";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <h1>Under Construction</h1>
+    <div class="hero-section">
+      <p>Spirits Auto</p>
+      <section>
+        <button>Avatar</button>
+        <button>Test Drive</button>
+      </section>
+      <nav>
+        <ul>
+          <li>Locations</li>
+          <li>Vehichles</li>
+          <li>Technology</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+      <section>
+        <h1>Rarest Collections unknown if outside the Loop</h1>
+        <p>Full speed ahead, lead by innovation and magical designs. Bring us your wildest dreams and start counting the days for delivery. Car or boat, chariot or nimbus. You are now limitless.</p>
+        <div>
+          <a>Catalog</a>
+          <a>Create a Model</a>
+        </div>
+      </section>
+    </div>
+    <div class="locations" id="locations">
+      <h1>ok</h1>
+    </div>
   </div>
 `;
 
